f220386a33948154ad33db2c1783f1e61ea42ad6
karishmakhurana97 <81068923+karishmakhurana97@users.noreply.github.com>
2021-08-16 12:48:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+//create variables here | global variable
+var box;
+
+function setup() {
+  createCanvas(400, 400);
+ // box=createSprite(200,200,50,50)
+}
+
+function draw() {
+  background("black");
+  drawSprites();
+}
